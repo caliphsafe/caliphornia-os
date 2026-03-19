a8859f543c3bfaa1675d22746ac9ca4ef0b7faf1
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-03-19 12:35:53 -0400

    Update AppIcon.tsx

--- components/AppIcon.tsx
+++ components/AppIcon.tsx
@@ -3,9 +3,10 @@ import type { AppItem } from "@/lib/app-registry";
 
 export default function AppIcon({ app }: { app: AppItem }) {
   return (
-    <Link href={app.href} className="app-icon">
-      <div className="app-icon-tile">
+    <Link href={app.href} className="app-icon ios-app-icon">
+      <div className="app-icon-tile ios-app-icon-tile">
         <img src={app.icon} alt={app.name} className="app-icon-image" />
+        <div className="icon-shine" />
       </div>
       <span className="app-icon-label">{app.name}</span>
     </Link>
