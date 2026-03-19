75dbd5f3eebe67cc2e4238525174faf4277a1bd7
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-03-19 11:41:50 -0400

    Create app-registry.ts

---
+++ lib/app-registry.ts
@@ -0,0 +1,27 @@
+export type AppItem = {
+  id: string;
+  name: string;
+  icon: string;
+  href: string;
+};
+
+export const appRegistry: AppItem[] = [
+  {
+    id: "fatherhood",
+    name: "FarTHErHOOD",
+    icon: "/icons/fatherhood.png",
+    href: "/apps/fatherhood"
+  },
+  {
+    id: "friends",
+    name: "frie.ends",
+    icon: "/icons/friends.png",
+    href: "/apps/friends"
+  },
+  {
+    id: "music",
+    name: "Music",
+    icon: "/icons/music.png",
+    href: "/apps/music"
+  }
+];
