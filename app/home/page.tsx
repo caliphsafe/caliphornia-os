c3aace22ca6f17adc0f53d8d0ec42f5467126e47
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-03-19 11:45:40 -0400

    Create page.tsx

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import HomeScreen from "@/components/HomeScreen";
+import { verifySession } from "@/lib/session";
+
+export default async function HomePage() {
+  const cookieStore = await cookies();
+  const session = verifySession(cookieStore.get("caliph_os_session")?.value);
+
+  if (!session) {
+    redirect("/");
+  }
+
+  return <HomeScreen email={session.email} />;
+}
